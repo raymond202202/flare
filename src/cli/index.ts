8a7b209cec9f610b6f0d0c62b04a87e31d95bb8b
raymond202202 <raymond202202@users.noreply.github.com>
2026-08-01 00:22:50 +0800

    ✨ v0.2.8 Banner 上方留一行空格

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -87,6 +87,7 @@ function renderBanner(): string {
 }
 
 async function startInteractive() {
+  console.log()  // 欢迎词上方留一行空格
   console.log(renderBanner())
   console.log(chalk.gray('输入 /help 查看命令，/exit 退出\n'))
 
